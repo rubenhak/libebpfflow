4d22bb71dd032e585340b66a0efa42a9b7f7f647
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-02-03 11:46:17 -0800

    working on builder docker image

---
+++ workstation/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+MY_DIR="$(dirname $MY_PATH)"
+REPO_DIR="$(dirname $MY_DIR)"
+cd $REPO_DIR
+
+echo "REPO_DIR=${REPO_DIR}"
+
+docker run -it --rm --privileged \
+  -v ${REPO_DIR}:/usr/src \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  ebpflib-workstation
