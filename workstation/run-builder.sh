58cd7271bd34b58b173f3a63ab6f02814878a4cb
rubenhak <ruben.hakopian@gmail.com>
2020-02-03 15:58:48 -0500

    Fixes to builder image

--- workstation/run-builder.sh
+++ workstation/run-builder.sh
@@ -9,4 +9,8 @@ echo "REPO_DIR=${REPO_DIR}"
 docker run -it --rm --privileged \
   -v ${REPO_DIR}:/usr/libbpfflow.git \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /sys/kernel/debug:/sys/kernel/debug \
+  -v /lib/modules:/lib/modules:ro \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /snap/bin/microk8s.ctr:/snap/bin/microk8s.ctr \
   ebpflib-workstation
